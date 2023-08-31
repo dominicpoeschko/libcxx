8fe500f22a3380b7282d7e4a3940db146095c4bc
dominic <mail@dominicp.de>
2023-08-31 16:10:12 +0200

    removed warnings

--- libcxx.cmake
+++ libcxx.cmake
@@ -6,17 +6,21 @@ set(
   src/ryu/f2s.cpp
 )
 
+set(libcxx_flags
+    -D_LIBCPP_BUILDING_LIBRARY
+    -Wno-reserved-id-macro
+    -Wno-atomic-alignment
+    -Wno-tautological-type-limit-compare
+    -Wno-double-promotion
+    -Wno-unused-parameter
+    -Wno-sign-conversion
+    -Wno-unsafe-buffer-usage
+    -Wno-ctad-maybe-unsupported
+    -Wno-covered-switch-default)
+
+list(JOIN libcxx_flags " " LIBCXX_FLAGS)
+
 list(TRANSFORM LIBCXX_SOURCE_FILES PREPEND "${CMAKE_CURRENT_LIST_DIR}/")
 
 set_source_files_properties(
-  ${LIBCXX_SOURCE_FILES} PROPERTIES COMPILE_FLAGS
-  "-D_LIBCPP_BUILDING_LIBRARY \
-    -Wno-reserved-id-macro \
-    -Wno-atomic-alignment \
-    -Wno-tautological-type-limit-compare \
-    -Wno-double-promotion \
-    -Wno-unused-parameter \
-    -Wno-sign-conversion \
-    -Wno-ctad-maybe-unsupported \
-    -Wno-covered-switch-default"
-)
+  ${LIBCXX_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${LIBCXX_FLAGS}")
