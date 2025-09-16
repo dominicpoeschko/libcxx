90f08a529c804aa0c8c8ce906e9e6d4da6d4d41f
dominic <mail@dominicp.de>
2025-09-16 23:56:23 +0200

    some warnings

--- libcxx.cmake
+++ libcxx.cmake
@@ -1,10 +1,4 @@
-set(
-  LIBCXX_SOURCE_FILES
-  src/charconv.cpp
-  src/ryu/d2fixed.cpp
-  src/ryu/d2s.cpp
-  src/ryu/f2s.cpp
-)
+set(LIBCXX_SOURCE_FILES src/charconv.cpp src/ryu/d2fixed.cpp src/ryu/d2s.cpp src/ryu/f2s.cpp)
 
 set(libcxx_flags
     -D_LIBCPP_BUILDING_LIBRARY
@@ -22,5 +16,4 @@ list(JOIN libcxx_flags " " LIBCXX_FLAGS)
 
 list(TRANSFORM LIBCXX_SOURCE_FILES PREPEND "${CMAKE_CURRENT_LIST_DIR}/")
 
-set_source_files_properties(
-  ${LIBCXX_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${LIBCXX_FLAGS}")
+set_source_files_properties(${LIBCXX_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${LIBCXX_FLAGS}")
